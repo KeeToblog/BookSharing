0f8a408407c395999f3f1d6a03e991ea98bae673
MakiseKeito <jus63@outlook.jp>
2020-01-16 14:37:13 +0900

    carrier_wave.rbのアクセスキーとシークレットキーを設定し直した

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,5 +1,9 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_public = false
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
@@ -7,7 +11,5 @@ if Rails.env.production?
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
-    config.fog_public = false
   end
 end
