bf7ee9ac006932b336624d99399ef055d9185c22
KeeToblog <jus63@outlook.jp>
2020-03-16 23:49:06 +0900

    AWSでWebアプリが動くように環境構築をした

--- config/puma.rb
+++ config/puma.rb
@@ -2,10 +2,17 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+
+# デーモン化の設定（アプリを長時間バックグラウンドで常駐させる）
+daemonize true
+pidfile "#{Dir.pwd}/tmp/pids/puma.pid"
+#stdout_redirect "#{Dir.pwd}/tmp/logs/puma.stdout.log", "#{Dir.pwd}/tmp/logs/puma.stderr.log", true
+
 preload_app!
 
 rackup      DefaultRackup
-port        ENV['PORT']     || 3000
+#port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
